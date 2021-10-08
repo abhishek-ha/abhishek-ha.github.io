7ee2d42d04fe29facc8bb7469a51f0f95e9594e4
Abhishek Kumar <leo.abhishek@gmail.com>
2021-10-09 00:05:32 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
 $(function () {
   $("#box4words").wordsrotator({
-     words: ['<img class="sliderb img-fluid" src="images/family.png" />','<img class="sliderb img-fluid" src="images/dental.png" />', '<img class="sliderb img-fluid" src="images/wellness-slider.png" />', '<img class="sliderb img-fluid" src="images/preventive-health.png" />','<img class="sliderb img-fluid" src="images/opthal.png" />','<img class="sliderb img-fluid" src="images/gynaecologists.png" />','<img class="sliderb img-fluid" src="images/shealth.png"/>'],
+     words: ['<img class="sliderb img-fluid" src="images/childcare.png" />', '<img class="sliderb img-fluid" src="images/eldercare.png" />', '<img class="sliderb img-fluid" src="images/gynaecologists.png" />', '<img class="sliderb img-fluid" src="images/preventive-health.png" />', '<img class="sliderb img-fluid" src="images/dental.png" />','<img class="sliderb img-fluid" src="images/opthal.png" />','<img class="sliderb img-fluid" src="images/sleep.png" />','<img class="sliderb img-fluid" src="images/weight-loss.png" />', '<img class="sliderb img-fluid" src="images/wellness-slider.png"/>','<img class="sliderb img-fluid" src="images/chestpain.png"/>','<img class="sliderb img-fluid" src="images/neckpain.png"/>','<img class="sliderb img-fluid" src="images/allergy.png" />','<img class="sliderb img-fluid" src="images/covid-concern.png" />','<img class="sliderb img-fluid" src="images/stomach-pain.png" />','<img class="sliderb img-fluid" src="images/shealth.png" />'],
     animationIn: "slideIn 0.4s",
     animationOut: "slideOut 0.4s",
-    speed: 4000
+    speed: 200000
   });;
 
   $("#box6words").wordsrotator({
- words: [' Children & Family', 'Dental', 'Wellness', 'Preventive HealthCare', 'Opthal',  'Gynaecologists', 'Sexual Health'],
+ words: [' Child Care <span  class="near">, near you</span>','Elderly Care <span class="near">, near you</span>','Women Care <span  class="near">, near you</span>','Health Screening <span  class="near">, near you</span>',  'Dental Checkup <span  class="near">, near you</span>','Eye Checkup <span  class="near">, near you</span>','Sleep Issues <span  class="near">, near you</span>','Weight Issues <span  class="near">, near you</span>', 'Mindfulness <span  class="near">, near you</span>','Chest Discomfort <span  class="near">, near you</span>','Neck Pain <span  class="near">, near you</span>','Flu/Allergy <span  class="near">, near you</span>','Covid Concerns <span  class="near">, near you</span>','Upset Stomach <span  class="near">, near you</span>', 'Therapy <span  class="near">, near you</span>'],
     animationIn: "slideIn 0.4s",
     animationOut: "slideOut 0.4s",
-    speed: 4000
+    speed: 200000
   });
-
 });
 
 
 
 $(function () {
   $("#box1words").wordsrotator({
-   words: ['<img class="sliderb img-fluid" src="images/family.png" />','<img class="sliderb img-fluid" src="images/dental.png" />', '<img class="sliderb img-fluid" src="images/wellness-slider.png" />', '<img class="sliderb img-fluid" src="images/preventive-health.png" />','<img class="sliderb img-fluid" src="images/opthal.png" />','<img class="sliderb img-fluid" src="images/gynaecologists.png" />','<img class="sliderb img-fluid" src="images/shealth.png"/>'],    
-   animationIn: "fadeInDown",
+   words: ['<img class="sliderb img-fluid" src="images/childcare.png" />', '<img class="sliderb img-fluid" src="images/eldercare.png" />', '<img class="sliderb img-fluid" src="images/gynaecologists.png" />', '<img class="sliderb img-fluid" src="images/preventive-health.png" />', '<img class="sliderb img-fluid" src="images/dental.png" />','<img class="sliderb img-fluid" src="images/opthal.png" />','<img class="sliderb img-fluid" src="images/sleep.png" />','<img class="sliderb img-fluid" src="images/weight-loss.png" />', '<img class="sliderb img-fluid" src="images/wellness-slider.png"/>','<img class="sliderb img-fluid" src="images/chestpain.png"/>','<img class="sliderb img-fluid" src="images/neckpain.png"/>','<img class="sliderb img-fluid" src="images/allergy.png" />','<img class="sliderb img-fluid" src="images/covid-concern.png" />','<img class="sliderb img-fluid" src="images/stomach-pain.png" />','<img class="sliderb img-fluid" src="images/shealth.png" />'],   animationIn: "fadeInDown",
     animationOut: "fadeOut",
     speed: 4000
   });;
 
   $("#box2words").wordsrotator({
-    words: [' Children & Family', 'Dental', 'Wellness', 'Preventive HealthCare', 'Opthal',  'Gynaecologists', 'Sexual Health'],
+ words: [' Child Care <span  class="near">, near you</span>','Elderly Care <span class="near">, near you</span>','Women Care <span  class="near">, near you</span>','Health Screening <span  class="near">, near you</span>',  'Dental Checkup <span  class="near">, near you</span>','Eye Checkup <span  class="near">, near you</span>','Sleep Issues <span  class="near">, near you</span>','Weight Issues <span  class="near">, near you</span>', 'Mindfulness <span  class="near">, near you</span>','Chest Discomfort <span  class="near">, near you</span>','Neck Pain <span  class="near">, near you</span>','Flu/Allergy <span  class="near">, near you</span>','Covid Concerns <span  class="near">, near you</span>','Upset Stomach <span  class="near">, near you</span>', 'Therapy <span  class="near">, near you</span>'],
     animationIn: "fadeInDown",
     animationOut: "fadeOut",
     speed: 4000
@@ -134,7 +132,7 @@ viewedSlider.owlCarousel(
 loop:true,
 margin:30,
 autoplay:true,
-autoplayTimeout:2500,
+autoplayTimeout:50000,
 nav:false,
 dots:false,
 responsive:
@@ -167,6 +165,11 @@ viewedSlider.trigger('next.owl.carousel');
 }
 
 });
+
+
+
+
+
 
 
 $(document).ready(function() {
@@ -213,17 +216,45 @@ $(document).ready(function() {
     $("#corporateslider").owlCarousel({
 loop:true,
 margin:30,
+arrows: true,
 autoplay:true,
+
 autoplayTimeout:3000,
    responsive:
 {
 0:{items:2},
 575:{items:3},
 768:{items:4},
-991:{items:3},
-1199:{items:3}
+991:{items:5},
+1199:{items:5}
 }
+
     });
 
+
+if($('.cor_prev').length)
+{
+var prev = $('.cor_prev');
+prev.on('click', function()
+{
+viewedSlider.trigger('prev.owl.carousel');
 });
+}
+
+if($('.cor_next').length)
+{
+var next = $('.cor_next');
+next.on('click', function()
+{
+viewedSlider.trigger('next.owl.carousel');
+});
+}
+
+
+
+
+});
+
+
+
 
