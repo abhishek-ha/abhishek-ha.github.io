f47cc2a5f0442caf8e7c10741233ff1c82aee952
Munish Kumar <munish.kumar@eton-solutions.com>
2022-01-24 15:41:00 +0530

    - Latest diagnostics uploaded

--- diagnostics/flutter_service_worker.js
+++ diagnostics/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "efdae676bffae2361dbcdcc934ce4875",
-"index.html": "14d32109269f81bb11f97c62b1132243",
-"/": "14d32109269f81bb11f97c62b1132243",
-"main.dart.js": "aceaea326755727fdab36d99e612daf7",
+"index.html": "ec4b08ef423dfcbf52466ee204ac9c39",
+"/": "ec4b08ef423dfcbf52466ee204ac9c39",
+"main.dart.js": "ef2f072e89d574a716bd9b9e70913253",
 "favicon.png": "04e710fbd264209d75c68aedd9674dda",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
