ce4d983c8c7849c0b17724266f80ff8c9925b37e
Deepak Kumar <deepak.kumar@healthassure.in>
2022-06-04 13:19:36 +0530

    Accenture-plan  with Email OTP Verification

--- js/buyplansui.js
+++ js/buyplansui.js
@@ -42,8 +42,8 @@ function getUrlVars1(url = window.location.href) {
     return vars;
 }
 var Vars1 = getUrlVars1();
-function OpenBuyPlanPopup(popuptitle, PlanCode, CouponCode, IsUserTracker = false) {
-    $('#BuyPlaniframe').attr('src', '../buyPlan.html?PopupTitle=' + popuptitle + '&PlanCode=' + PlanCode + '&CouponCode=' + CouponCode + '&IsUserTracker=' + IsUserTracker + '&utm_source=' + Vars1["utm_source"] + '&utm_medium=' + Vars1["utm_medium"] + '&utm_campaign=' + Vars1["utm_campaign"] + '&utm_term=' + Vars1['utm_term'] + '&utm_content=' + Vars1['utm_content']);
+function OpenBuyPlanPopup(popuptitle, PlanCode, CouponCode, IsUserTracker = false, IsVarifyEmail = false, EmailDomain = "") {
+    $('#BuyPlaniframe').attr('src', '../buyPlan.html?PopupTitle=' + popuptitle + '&PlanCode=' + PlanCode + '&CouponCode=' + CouponCode + '&IsUserTracker=' + IsUserTracker + '&utm_source=' + Vars1["utm_source"] + '&utm_medium=' + Vars1["utm_medium"] + '&utm_campaign=' + Vars1["utm_campaign"] + '&utm_term=' + Vars1['utm_term'] + '&utm_content=' + Vars1['utm_content'] + '&IsVarifyEmail=' + IsVarifyEmail + '&EmailDomain=' + EmailDomain);
     openmodal();
 }
 
