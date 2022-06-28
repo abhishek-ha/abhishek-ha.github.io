5fa53b10b1bd73600342e029ce471f24537d2ec1
Deepak Kumar <deepak.kumar@healthassure.in>
2022-06-28 15:19:15 +0530

    add Password-reset page

---
+++ js/Password-reset.js
@@ -0,0 +1,35 @@
+function PasswordValidation(password) {
+    return new Promise(function (resolve, reject) {
+        var href = new URL(window.location.href);
+        var userId = href.searchParams.get("UserId");
+        var source = href.searchParams.get("Source");
+        if (userId == null || userId == "" || source == null || source == "") {
+            reject("invalid Url")
+            return;
+        }
+        let userpass = { NewPassword: password, UserId: userId, Source: source };
+        $.ajax({
+            type: "POST",
+            url: "https://uat.healthassure.in/productapi/api/User/ChangingPassword",
+            dataType: "json",
+            data: JSON.stringify(userpass),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then() 
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            },
+            error: function (eror) {
+                console.warn(eror);
+                reject(eror) // Reject the promise and go to catch()
+            }
+        });
+    });
+}
