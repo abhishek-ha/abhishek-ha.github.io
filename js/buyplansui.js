af4f778fd25cd5e71c537f8988ab94f8bbb42e30
Deepak Kumar <deepak.kumar@healthassure.in>
2022-07-20 13:36:45 +0530

    marge branch

--- js/buyplansui.js
+++ js/buyplansui.js
@@ -29,7 +29,7 @@ function getUrlVars1(url = window.location.href) {
 }
 var Vars1 = getUrlVars1();
 function OpenBuyPlanPopup(popuptitle, PlanCode, CouponCode, IsUserTracker = false, IsVarifyEmail = false, EmailDomain = "") {
-    $('#BuyPlaniframe').attr('src', 'file:///F:/website%20work/HaWebsite/abhishek-ha.github.io/buyPlan.html?PopupTitle=' + popuptitle + '&PlanCode=' + PlanCode + '&CouponCode=' + CouponCode + '&IsUserTracker=' + IsUserTracker + '&utm_source=' + Vars1["utm_source"] + '&utm_medium=' + Vars1["utm_medium"] + '&utm_campaign=' + Vars1["utm_campaign"] + '&utm_term=' + Vars1['utm_term'] + '&utm_content=' + Vars1['utm_content'] + '&IsVarifyEmail=' + IsVarifyEmail + '&EmailDomain=' + EmailDomain);
+    $('#BuyPlaniframe').attr('src', 'https://healthassure.in/buyPlan.html?PopupTitle=' + popuptitle + '&PlanCode=' + PlanCode + '&CouponCode=' + CouponCode + '&IsUserTracker=' + IsUserTracker + '&utm_source=' + Vars1["utm_source"] + '&utm_medium=' + Vars1["utm_medium"] + '&utm_campaign=' + Vars1["utm_campaign"] + '&utm_term=' + Vars1['utm_term'] + '&utm_content=' + Vars1['utm_content'] + '&IsVarifyEmail=' + IsVarifyEmail + '&EmailDomain=' + EmailDomain);
     openmodal();
 }
 
