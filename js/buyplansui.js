fd61f1182ae2517894301f0054c8b760b4b1a2b1
Deepak Kumar <deepak.kumar@healthassure.in>
2022-07-20 12:43:07 +0530

    chnages for buy plan ui

--- js/buyplansui.js
+++ js/buyplansui.js
@@ -1,6 +1,6 @@
 (function () {
     $('body').prepend("<div style=' background-color: rgba(0, 0, 0, 0.5);'> <style>"
-        +".popup-wrapper{display:none;position:absolute;z-index:999;padding-top:10px;padding-bottom:30px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(0,0,0,.4)}@media only screen and (max-width:600px){.popup-content,.popup-content1{margin:auto;padding:20px 20px 70px;border:0 solid #888;width:70%;position:relative;min-height:84%}}@media only screen and (min-width:601px){.popup-content,.popup-content1{margin:auto auto 30px;padding:20px 20px 70px;border:0 solid #888;width:40%;position:relative;min-height:84%}}"+
+        +".popup-wrapper{display:none;position:fixed;z-index:99999;padding-top:10px;padding-bottom:30px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(0,0,0,.4)}@media only screen and (max-width:600px){.popup-content{margin:auto auto 30px;padding:20px 20px 70px;border:0 solid #888;width:40%;position:relative;min-height:84%}}@media only screen and (min-width:601px){.popup-content,.popup-content1{margin:auto auto 30px;padding:20px 20px 70px;border:0 solid #888;width:40%;position:relative;min-height:84%}}"+
         "            @keyframes animatetop {" +
         "                from {" +
         "                    top: -300px;" +
@@ -15,7 +15,7 @@
         "        </style>" +
         "        <div id='buyplanpopup' class='popup-wrapper' >" +
         "            <div class='popup-content'>" +
-        "<iframe id='BuyPlaniframe' frameborder='0' allowtransparency='true' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px' height='100%' width='100%' ></iframe> </div></div></div>");
+        "<iframe id='BuyPlaniframe' frameborder='0' allowtransparency='true' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:fixed;top:0px;left:0px;right:0px;bottom:0px' height='100%' width='100%' ></iframe> </div></div></div>");
 })();
 function getUrlVars1(url = window.location.href) {
     var vars = [], hash;
