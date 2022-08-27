e723ac823926c1fa2e565ac65c426b2972ad3dde
Deepak Kumar <deepak.kumar@healthassure.in>
2022-08-27 17:28:22 +0530

    set password page changes

--- js/setpassword.js
+++ js/setpassword.js
@@ -1,8 +1,8 @@
 function PasswordValidation(password) {
     return new Promise(function (resolve, reject) {
         var href = new URL(window.location.href);
-        var userId = href.searchParams.get("U");
-        var source = href.searchParams.get("S");
+        var userId = href.searchParams.get("UserId");
+        var source = href.searchParams.get("Source");
         if (userId == null || userId == "" || source == null || source == "") {
             reject("invalid Url")
             return;
