a1bfbcb401decfff4e15ae20438b888034faa9da
Deepak Kumar <deepak.kumar@healthassure.in>
2022-10-06 17:17:05 +0530

    add logrocket in opd buy plan

--- js/buyplans.js
+++ js/buyplans.js
@@ -10,6 +10,7 @@
 
 function BuyOPDPlan(Name, Email, MobileNo, DOB, Gender, PlanCode, CouponCode, description, utm_Source, utm_Medium, utm_Campaign, utm_Term, utm_Content, Relations, Agent) {
     return new Promise(function (resolve, reject) {
+        AddLogRocket(Email, { PlanCode: PlanCode, MobileNo: MobileNo, Description: description });
         var BaseUrl = "https://live.healthassure.in/ProductApi/"
         var headers = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak' };
         $.ajax({
@@ -17,75 +18,94 @@ function BuyOPDPlan(Name, Email, MobileNo, DOB, Gender, PlanCode, CouponCode, de
             data: JSON.stringify({ Name: Name, Email: Email, MobileNo: MobileNo, DOB: DOB, Gender: Gender, PlanCode: PlanCode, CouponCode: CouponCode, Status: 'BuyRequest', utmSource: utm_Source, utmMedium: utm_Medium, utmCampaign: utm_Campaign, utmTerm: utm_Term, utmContent: utm_Content, Relations: Relations, Agent: Agent }),
             dataType: "json", headers: headers,
             success: function (result) {
-                if (PlanCode == "HAOPDR003-60") {
-                    try {
-                        ShowLoding(false);
-                        opningPaymentSuccesspage();
-                        return;
-                    } catch { }
-                }
-                if (result.status) {
-                    var options = {
-                        "key": result.results.razorpayKey,
-                        "amount": result.results.transaction.amountIncludingGST,
-                        "currency": result.results.razorpayOrder.currency,
-                        "name": "HealthAssure",
-                        "description": description,
-                        "image": BaseUrl + "image/logo/halogo.png",
-                        "order_id": result.results.razorpayOrder.id,
-                        "handler": function (response) {
-                            try {
-                                ShowLoding(true);
-                            } catch { }
-                            $.ajax({
-                                type: "POST", url: BaseUrl + "api/OPDPlans/BuyOPDPlanConformationWithRazorpay",
-                                data: JSON.stringify({ TransactionId: result.results.razorpayOrder.receipt, Message: "Payment Success", OrderId: result.results.razorpayOrder.id, Signature: response.razorpay_signature, RefId: response.razorpay_payment_id, Status: 'Success' }),
-                                dataType: "json", headers: headers,
-                                success: function (res) {
-                                    if (res.status) {
-                                        resolve(res) // Resolve promise and go to then() 
-                                        try {
-                                            ShowLoding(false);
-                                            opningPaymentSuccesspage();
-                                        } catch { }
-                                    } else {
-                                        console.warn(res); reject("Error from Buy OPD Plan Conformation")
-                                    }
-                                },
-                                error: function (error) {
-                                    console.warn(error); reject(error) // Reject the promise and go to catch()
-                                }
-                            });
-                        },
-                        "prefill": { "name": Name, "email": Email, "contact": "91" + MobileNo },
-                        "notes": {
-                            "address": "Registered Office : 1st Floor, Excom House, North Wing, 7 Saki-vihar Road, Sakinaka, Andheri(E), Mumbai-400 072"
-                        },
-                        "theme": { "color": "#3399cc" },
-                        "modal": {
-                            "ondismiss": function () {
+                try {
+                    if (PlanCode == "HAOPDR003-60") {
+                        try {
+                            ShowLoding(false);
+                            opningPaymentSuccesspage();
+                            return;
+                        } catch { }
+                    }
+                    AddLogRocket(Email, { PlanCode: PlanCode, log: "Paymant initiated" });
+                    if (result.status) {
+                        var options = {
+                            "key": result.results.razorpayKey,
+                            "amount": result.results.transaction.amountIncludingGST,
+                            "currency": result.results.razorpayOrder.currency,
+                            "name": "HealthAssure",
+                            "description": description,
+                            "image": BaseUrl + "image/logo/halogo.png",
+                            "order_id": result.results.razorpayOrder.id,
+                            "handler": function (response) {
+                                AddLogRocket(Email, { PlanCode: PlanCode, log: "Paymant Success" });
                                 try {
-                                    userClosePaymentPopup();
+                                    ShowLoding(true);
                                 } catch { }
+                                $.ajax({
+                                    type: "POST", url: BaseUrl + "api/OPDPlans/BuyOPDPlanConformationWithRazorpay",
+                                    data: JSON.stringify({ TransactionId: result.results.razorpayOrder.receipt, Message: "Payment Success", OrderId: result.results.razorpayOrder.id, Signature: response.razorpay_signature, RefId: response.razorpay_payment_id, Status: 'Success' }),
+                                    dataType: "json", headers: headers,
+                                    success: function (res) {
+                                        if (res.status) {
+                                            AddLogRocket(Email, { PlanCode: PlanCode, log: "plan purchased" });
+                                            resolve(res) // Resolve promise and go to then() 
+                                            try {
+                                                ShowLoding(false);
+                                                opningPaymentSuccesspage();
+                                            } catch { }
+                                        } else {
+                                            AddLogRocket(Email, { PlanCode: PlanCode, log: "api=> api/OPDPlans/BuyOPDPlanConformationWithRazorpay, Error ", result: res });
+                                            console.warn(res); reject("Error from Buy OPD Plan Conformation")
+                                        }
+                                    },
+                                    error: function (error) {
+                                        AddLogRocket(Email, { PlanCode: PlanCode, log: "api=> api/OPDPlans/BuyOPDPlanConformationWithRazorpay, calling Error ", TransactionId: TransactionId });
+                                        console.warn(error); reject(error) // Reject the promise and go to catch()
+                                    }
+                                });
+                            },
+                            "prefill": { "name": Name, "email": Email, "contact": "91" + MobileNo },
+                            "notes": {
+                                "address": "Registered Office : 1st Floor, Excom House, North Wing, 7 Saki-vihar Road, Sakinaka, Andheri(E), Mumbai-400 072"
+                            },
+                            "theme": { "color": "#3399cc" },
+                            "modal": {
+                                "ondismiss": function () {
+                                    try {
+                                        userClosePaymentPopup();
+                                        AddLogRocket(Email, { PlanCode: PlanCode, log: "ondismiss => userClosePaymentPopup" });
+                                    } catch { }
+                                }
                             }
-                        }
-                    };
-                    var rzp1 = new Razorpay(options);
-                    rzp1.on('payment.failed', function (response) {
-                        console.warn(response.error);
-                    });
-                    rzp1.open();
-                    try {
-                        opningPaymentpage();
-                    } catch { }
-                } else {
-                    console.warn(result); reject('Error Creating transaction Id from HealthAssure side') // Reject the promise and go to catch()
+                        };
+                        var rzp1 = new Razorpay(options);
+                        rzp1.on('payment.failed', function (response) {
+                            console.warn(response.error);
+                        });
+                        rzp1.open();
+                        try {
+                            opningPaymentpage();
+                        } catch { }
+                    } else {
+                        AddLogRocket(Email, { PlanCode: PlanCode, log: "api=> api/OPDPlans/BuyOPDPlanWithRazorpay, Error ", result: result });
+                        console.warn(result); reject('Error Creating transaction Id from HealthAssure side') // Reject the promise and go to catch()
+                    }
+                } catch (e) {
+                    AddLogRocket(Email, { PlanCode: PlanCode, log: "api=> api/OPDPlans/BuyOPDPlanWithRazorpay, Calling Error " });
+                    console.log(e);
                 }
             },
             error: function (eror) {
+                AddLogRocket(Email, { PlanCode: PlanCode, log: "api=> api/OPDPlans/BuyOPDPlanWithRazorpay, Calling Error " });
                 console.warn(eror); reject(eror) // Reject the promise and go to catch()
             }
         });
     });
 }
 
+function AddLogRocket(email, objectLog) {
+    try {
+        LogRocket.identify(email, objectLog);
+        LogRocket.log(email, objectLog);
+    } catch { }
+}
