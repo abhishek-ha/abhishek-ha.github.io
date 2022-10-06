a97db1602aa2da74f676b8a8cbfedcfb9efc26e1
Deepak Kumar <deepak.kumar@healthassure.in>
2022-10-06 19:58:36 +0530

    commit

--- BuyPlan/js/AccessCodeRegistration.js
+++ BuyPlan/js/AccessCodeRegistration.js
@@ -17,7 +17,7 @@
         "            <div class='popup-content'>" +
         "<iframe id='Registrationiframe' frameborder='0' allowtransparency='true' style='overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:fixed;top:0px;left:0px;right:0px;bottom:0px' height='100%' width='100%' ></iframe> </div></div></div>");
 })();
-function OpenBuyPlanPopup(popuptitle, IsEnableTncDeclaration = false, IsEnableMembersAddtion = false) {
+function OpenAccessCodeRegPopup(popuptitle, IsEnableTncDeclaration = false, IsEnableMembersAddtion = false) {
     $('#Registrationiframe').attr('src', 'https://live.healthassure.in/buyPlan/AccessCodeRegistration.html?PopupTitle=' + popuptitle +  '&IsEnableTncDeclaration=' + IsEnableTncDeclaration + '&IsEnableMembersAddtion=' + IsEnableMembersAddtion );
     openmodal();
 }
