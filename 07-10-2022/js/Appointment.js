9ab7ec37078cfba394cb65288e73dce938fdb769
avanishshukla1 <31564169+avanishshukla1@users.noreply.github.com>
2022-10-07 15:42:37 +0530

    fitassure

---
+++ 07-10-2022/js/Appointment.js
@@ -0,0 +1,137 @@
+var reportsPath = '';
+var href = new URL(window.location.href);
+var Appointmentid = href.searchParams.get("AppointmentId");
+function maplocation(lat, long) {
+    window.open('https://maps.google.com/?q=' + lat + ',' + lat);
+}
+$('#btnViewReports').click(function () {
+    window.open(reportsPath);
+});
+function LoadData(Appointmentid) {
+    $.ajax({
+        url: "https://uat.healthassure.in/productApi/api/UserOPDPlans/AppointmentBoardingPass?Appointmentid=" + Appointmentid,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+        },
+        success: function (data) {
+            console.log('success');
+            if (data.status && data.results != null) {
+                document.title = ' DI-Appointment ' + data.results.statusname ;
+                $("#txtMemberName").html('Hi, ' + data.results.memberName);
+                $("#btnGender").text(data.results.gender);
+                $("#txtAppointmentDate").html('<img src="images/calendar-black.png" class="boxicon"> ' + data.results.appointmentDate);
+                // $("#txtAppointmentDate").attr('class','abc');
+                $("#txtAppointmentTime").html(' <img src="images/clock.png" class="boxicon">  ' + data.results.appointmentTime);
+                // $("#txtAddress").html(data.results.address + ' <img src="images/google-map.png" width="15" onclick="maplocation(' + data.results.providerLatitude + ',' + data.results.providerLongitude + ')">');
+                $("#txtAddress").html(data.results.address + ' <a href="javascript:maplocation(' + data.results.providerLatitude + ',' + data.results.providerLongitude + ')"><img src="images/google-map.png" class="boxicon"> Direction </a>');
+                $("#btnAge").text(data.results.memberAge + ' Years');
+                $("#btnCaseNo").text(data.results.caseNo);
+                $("#btnCaseNoConfirmed").text(data.results.caseNo);
+                $("#btnFasting").text(data.results.isFasting);
+                $("#txtDurationForTest").html(' <i class="fa-regular fa-clock boxicon"></i> ' + data.results.durationforTest);
+                $("#txtPaymentMode").html('<img src="images/wallet.png" class="boxicon"> '  + data.results.paymentMode);
+                $("#txtPreprationForCheckup").text(data.results.preprationForCheckup);
+                $("#btnBreakfastCentre").text(data.results.breakfastCentre);
+                $("#btnTechnician").text(data.results.technicianGender);
+                $("#btnReportSharing").text(data.results.reportSharingwithCustomer);
+                $("#btnPaymentSettlementMode").html('<img src="images/wallet.png" class="boxicon1"> ' + data.results.paymentSettlementMode);
+                $("#txtProviderName").text(data.results.providerName);
+                $("#txtProviderMobile").html('<img src="images/call-black-icon.png" class="boxicon"> ' + data.results.providerMobile);
+                $("#txtProviderEmail").html('<i class="fa-regular fa-envelope" ></i> ' + data.results.providerEmail);
+                $("#txtPointOfContact").text(data.results.PointOfContact);
+                if (data.results.reportSavePath != null) {
+                    reportsPath = data.results.reportSavePath;
+                }
+              if (data.results.providerImage == null) {
+                    $("#imgProviderImage").hide();
+                } else {
+                    $("#imgProviderImage").html('<img src="' + data.results.providerImage + 'images/apollo-logo.png" class="img-fluid" alt="">');
+                }
+                if (data.results.PointOfContact == null) {
+                    $("#dvPointOfContact").hide();
+                } if (data.results.durationforTest == null) {
+                    $("#dvDurationForTest").hide();
+                } if (data.results.preprationForCheckup == null) {
+                    $("#dvPreprationForCheckup").hide();
+                }
+                if (data.results.coveredTests != null && data.results.coveredTests.length > 0) {
+                    if (data.results.coveredTests.length = 1) {
+                        $.each(data.results.coveredTests, function (key, value) {
+                            // $('#ulTest').append('<li><i class="fa-solid fa-circle-check text-success"></i>  <a href="#">' + value.testName + ' </a> </li>');
+                            $("#txtplanName").html('<i class="fa-solid fa-circle-check text-success"></i> ' + value.testName);
+                        });
+                    }
+                    else {
+                        $("#txtplanName").html('<i class="fa-solid fa-circle-check text-success"></i> ' + data.results.planName);
+                        $("#dvTestListBtn").show();
+
+                        $.each(data.results.coveredTests, function (key, value) {
+                            $('#ulTest').append('<li><i class="fa-solid fa-circle-check text-success"></i>  <a href="#">' + value.testName + ' </a> </li>');
+                        });
+                    }
+                   }
+                else {
+                    $("#txtplanName").html('<i class="fa-solid fa-circle-check text-success"></i> ' + data.results.planName);
+                }
+                if (data.results.statusname == 'Cancelled') {
+                    $("#btnstatusname").text(' Appoinment ' + data.results.statusname);
+                    $("#btnstatusname").attr('class', 'btn btn-red statusbtn');
+                    $("#dvForCancelImage").show();
+                }
+                else if (data.results.statusname == 'Requested') {
+                    $("#btnstatusname").text(' Appoinment ' + data.results.statusname);
+                    $("#btnstatusname").attr('class', 'btn btn-grey');
+                    $("#dvForRequestImage").show();
+                }
+                else if (data.results.statusname == 'Completed' || data.results.statusname == 'Closed') {
+                    $("#btnstatusname").text(' Appoinment ' + data.results.statusname);
+                    $("#btnstatusname").attr('class', 'btn btn-grey');
+                    $("#dvForCompletedImage").show();
+                }
+                else if (data.results.statusname == 'Confirmed') {
+                    $("#btnstatusname").text(' Appoinment ' + data.results.statusname);
+                    $("#btnstatusname").attr('class', 'btn btn-orange statusbtn');
+                    $("#dvAppointmentConfirmed").show();
+                    $("#dvForConfirm").show();;
+                }
+                else {
+                    console.log("Error in status not matched");
+                    window.open("404.html");
+                }
+            } else {
+                console.log("Error in sucess");
+                window.location.replace("404.html");
+            }
+        },
+        error: function () {
+            console.log("Error");
+            window.location.replace("404.html");
+            swal('Error occure');
+        }
+    });
+}
+
+$(function () {
+    $("#dvTestListBtn").show();
+    $("#dvAppointmentConfirmed").hide();
+    $("#dvForConfirm").hide();
+    $("#dvForCancelImage").hide();
+    $("#dvForRequestImage").hide();
+    $("#dvForCompletedImage").hide();
+    $("#dvCaseNo").hide();
+    debugger;
+    if (Appointmentid != undefined && Appointmentid != null) {
+        debugger;
+        LoadData(Appointmentid);
+        $('body').show();
+    }
+    else {
+        window.location.replace("404.html");
+    }
+    console.log(Appointmentid);
+    console.log("ready!");
+});
---
+++ 07-10-2022/js/InsuranceAgent.js
@@ -0,0 +1,247 @@
+// JavaScript source code
+const APiBaseURL = 'https://live.healthassure.in/ProductApi';
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://checkout.razorpay.com/v1/checkout.js';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+
+function GetPartnerPlanDetails(PartnerCode) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "GET",
+            url: APiBaseURL + '/api/InsuranceAgent/GetPartnerPlanDetails?PartnerCode=' + PartnerCode,
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            },
+            error: function (eror) {
+                console.warn(eror);
+                reject(err) // Reject the promise and go to catch()
+            }
+        });
+    });
+}
+
+function CreateOrderOPDPlans(Name, Email, MobileNo, DOB, Gender, PlanCode, Relations, UserAddress, OPDPartnerDetailsId) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "POST",
+            url: APiBaseURL + '/api/OPDPlans/OPDPlansPurchase',
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'PartnerOPDPlanApiKey': '14UGczJo0lYOe5ilSzlT2cvLTj1lymz2lRWaBCwIQMd=pnIKeTDBAYApRXj6xO'
+            },
+            data: JSON.stringify({ Name: Name, Email: Email, MobileNo: MobileNo, DOB: DOB, Gender: Gender, PlanCode: PlanCode, Relations: Relations, UserAddress: UserAddress, IsSendPaymentLink: true, PartnerOrderId: OPDPartnerDetailsId }),
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            },
+            error: function (eror) {
+                console.warn(eror);
+                reject(err) // Reject the promise and go to catch()
+            }
+        });
+    });
+}
+
+function GetTransactionDetails(TransactionId) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "GET",
+            url: APiBaseURL + '/api/InsuranceAgent/GetTransactionDetails?TransactionId=' + TransactionId,
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            }
+        });
+    });
+}
+
+function UpdateTransactionDetais(OpdPayment) {
+    return new Promise(function (resolve, reject) {
+        debugger;
+        $.ajax({
+            type: "POST",
+            url: APiBaseURL + '/api/OPDPlans/UpdateOPDPaymentDetail',
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            data: JSON.stringify(OpdPayment),
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            }
+        });
+    });
+}
+
+function VerifyEmailId(MobileNumbar, OTP, EmailId, Source) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "POST",
+            url: APiBaseURL + '/api/User/VerifyEmailId',
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            data: JSON.stringify({ MobileNumbar: MobileNumbar, OTP: OTP, Source: Source, EmailId: EmailId }),
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            },
+            error: function (eror) {
+                console.warn(eror);
+                reject(err) // Reject the promise and go to catch()
+            }
+        });
+    });
+}
+
+function GetOTPForEmailId(Emailid, Source, MobileNo, PlanCode) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "GET",
+            url: APiBaseURL + '/api/User/GetOTPForEmailId?EmailId=' + Emailid + '&Source=' + Source + '&MobileNo=' + MobileNo + '&TriggerType=PLANPURCHASEVERIFYOTP' + '&PlanCode=' + PlanCode,
+            dataType: "json",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'ApiKey': 'joxNjM0MjE2NDQ5fQ.nk2tgCC1NRAbaperiPWQXXoNgybL27zdN3T4dC5L-ak'
+            },
+            success: function (result) {
+                console.warn(result.message);
+                if (result.status) {
+                    resolve(result) // Resolve promise and go to then()
+                } else {
+                    reject(result) // Reject the promise and go to catch()
+                }
+            },
+            error: function (eror) {
+                console.warn(eror);
+                reject(err) // Reject the promise and go to catch()
+            }
+        });
+    });
+}
+
+function BuyOPDPlan(TransactionId, razorpaykey, amount, order_id, description, Name, Email, MobileNo) {
+    return new Promise(function (resolve, reject) {
+        var headers = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'PartnerOPDPlanApiKey': '14UGczJo0lYOe5ilSzlT2cvLTj1lymz2lRWaBCwIQMd=pnIKeTDBAYApRXj6xO' };
+        var options = {
+            "key": razorpaykey,
+            "amount": amount,
+            "currency": "INR",
+            "name": "HealthAssure",
+            "description": description,
+            "image": APiBaseURL + "/image/logo/halogo.png",
+            "order_id": order_id,
+            "handler": function (response) {
+                AddLogRocket(Email, { TransactionId: TransactionId, log: "Paymant Success" });
+                $.ajax({
+                    type: "POST", url: APiBaseURL + "/api/OPDPlans/OPDPlansPurchaseConformation",
+                    data: JSON.stringify({ TransactionId: TransactionId, Message: "Payment Success", OrderId: order_id, Signature: response.razorpay_signature, RefId: response.razorpay_payment_id, Status: 'Success' }),
+                    dataType: "json", headers: headers,
+                    success: function (res) {
+                        if (res.status) {
+                            AddLogRocket(Email, { TransactionId: TransactionId, log: "plan purchased" });
+                            console.log("OPDPlansPurchaseConformation");
+                            try {
+                                ShowLoding(false);
+                                opningPaymentSuccesspage();
+                            } catch { }
+                            resolve(res) // Resolve promise and go to then()
+                        } else {
+                            AddLogRocket(Email, { TransactionId: TransactionId, log: "api=> api/OPDPlans/OPDPlansPurchaseConformation, Error ", result: res });
+                            console.warn(res); reject("Error from Buy OPD Plan Conformation")
+                        }
+                    },
+                    error: function (error) {
+                        AddLogRocket(Email, { TransactionId: TransactionId, log: "api=> api/OPDPlans/OPDPlansPurchaseConformation, calling Error " });
+                        console.warn(error); reject(error) // Reject the promise and go to catch()
+                    }
+                });
+            },
+            "prefill": { "name": Name, "email": Email, "contact": "91" + MobileNo },
+            "notes": {
+                "address": "Registered Office : 1st Floor, Excom House, North Wing, 7 Saki-vihar Road, Sakinaka, Andheri(E), Mumbai-400 072"
+            },
+            "theme": { "color": "#3399cc" },
+            "modal": {
+                "ondismiss": function () {
+                    AddLogRocket(Email, { TransactionId: TransactionId, log: "ondismiss => userClosePaymentPopup" });
+                }
+            }
+        };
+        try {
+            var rzp1 = new Razorpay(options);
+            rzp1.on('payment.failed', function (response) {
+                try {
+                    AddLogRocket(Email, { TransactionId: TransactionId, log: "error => payment.failed", response: response });
+                    ShowLoding(false);
+                    opningPaymentfailedpage();
+                } catch { }
+                reject(response)
+                console.warn(response.error);
+            });
+            rzp1.open();
+            AddLogRocket(Email, { TransactionId: TransactionId, log: "Open Payment popup" });
+            resolve("done");
+        } catch (e) {
+            reject(e);
+            try {
+                ShowLoding(false);
+                opningPaymentfailedpage();
+            } catch { }
+        }
+    });
+}
+
+function AddLogRocket(email, objectLog) {
+    try {
+        LogRocket.identify(email, objectLog);
+        LogRocket.log(email, objectLog);
+    } catch { }
+}
