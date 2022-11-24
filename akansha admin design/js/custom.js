42dab2b6286837f8a7ed12fe06b7ae6463520bef
avanishshukla1 <31564169+avanishshukla1@users.noreply.github.com>
2022-11-24 16:15:32 +0530

    design

--- akansha admin design/js/custom.js
+++ akansha admin design/js/custom.js
@@ -2,20 +2,3 @@ function toggleMenu() {
     var element = document.getElementById("sidebar-menu");
     element.classList.toggle("open");
 }
-
-
-// start date picker
-
-
-$(document).ready(function(){
-
-$('.datepicker').datepicker({
-    format: 'dd-mm-yyyy',
-    todayHighlight: true,
-    toggleActive: true
-});
-
-});
-
-// End date
-
