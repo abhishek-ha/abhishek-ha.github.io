15525dd78002dc72a1fc135790895c4ab806e773
Deepak Kumar <deepak.kumar@healthassure.in>
2023-03-01 17:41:12 +0530

    bording pass for pharmacy

--- hausers/js/pharmacy-order.js
+++ hausers/js/pharmacy-order.js
@@ -5,15 +5,10 @@ var place = '';
 
 
 $(function () {
-    $('#dvCenterUseOnly').hide();
-    $('#dvOrderCompleted').hide();
-    $('#dvOutDelivery').hide();
-    $('#dvPaymentDue').hide();
-    $('#dvOrderRequested').hide();
     if (Appointmentid != undefined && Appointmentid != null) { LoadData(Appointmentid); }
     else {
         console.log('error')
-        //window.location.replace("404.html");
+        window.location.replace("404.html");
     }
     console.log(Appointmentid);
     console.log("ready!");
@@ -23,7 +18,7 @@ $(function () {
 
 function LoadData(Appointmentid) {
     $.ajax({
-        url: "https://uat.healthassure.in/productApi/api/UserOPDPlans/AppointmentBoardingPass?Appointmentid=" + Appointmentid,
+        url: "https://uat.healthassure.in/ProductApi/api/UserOPDPlans/AppointmentBoardingPass?Appointmentid=" + Appointmentid,
         type: "GET",
         dataType: "json",
         headers: {
@@ -39,39 +34,63 @@ function LoadData(Appointmentid) {
                 place = data.results.appointmentAddress;
 
                 if (statusName == 'Requested') {
-                    document.title = ' Pharmacy | Order Requested';
-                    $('#dvCenterUseOnly').hide();
-                    $('#dvOrderRequested').show();
-                    $("#btnstatusname").attr('class', 'btn appoinmentbtn');
+                    document.title = 'Pharmacy | Order Requested';
+                    $("[id*='dvOrderRequested']").show();
+                    $("[id*='btnstatusname']").html('ORDER REQUESTED');
+                    $("[id*='btnstatusname']").attr('class', 'btn appoinmentbtn');
                 }
-                else if (statusName == 'Closed') {
-                    document.title = ' Pharmacy | Order Closed';
-                    $('#dvCenterUseOnly').show();
-                    $('#dvOrderCompleted').show();
-                    $("#btnstatusname").attr('class', 'btn btn-orange');
-
-                }
-                else if (statusName == 'Delivered') {
-                    document.title = ' Pharmacy | Order Delivered';
-                    $('#dvCenterUseOnly').show();
-                    $('#dvOutDelivery').show();
-                    $("#btnstatusname").attr('class', 'btn paymentduebtn');
+                else if (statusName == 'Dispatched' || statusName == 'Package Picked') {
+                    document.title = ' Pharmacy | Order Out For Delivery';
+                    $("[id*='dvOutDelivery']").show();
+                    $("[id*='dvCenterUseOnly']").show();
+                    $("[id*='dvconfirmPaidBy']").show();
+                    $("[id*='accordionFlushExample']").show();
+                    $("[id*='btnstatusname']").html('OUT FOR DELIVERY');
+                    $("[id*='btnstatusname']").attr('class', 'btn btn-outdelivery');
                 }
                 else if (statusName == 'Payment Pending') {
                     document.title = ' Pharmacy | Order Payment Due';
-                    $('#dvCenterUseOnly').show();
-                    $('#dvPaymentDue').show();
-                    $("#btnstatusname").attr('class', 'btn paymentduebtn');
+                    $("[id*='dvPaymentDue']").show();
+                    $("[id*='dvCenterUseOnly']").show();
+                    $("[id*='accordionFlushExample']").show();
+                    $("[id*='btnstatusname']").html('PAYMENT DUE');
+                    $("[id*='btnstatusname']").attr('class', 'btn paymentduebtn');
+                }
+                else if (statusName == 'Confirmed') {
+                    document.title = ' Pharmacy | Confirm';
+                    $("[id*='dvOrderConfirmed']").show();
+                    $("[id*='dvCenterUseOnly']").show();
+                    $("[id*='dvconfirmPaidBy']").show();
+                    $("[id*='accordionFlushExample']").show();
+                    $("[id*='btnstatusname']").html('ORDER CONFIRMED');
+                    $("[id*='btnstatusname']").attr('class', 'btn btn-outdelivery');
+                }
+                else if (statusName == 'Closed' || statusName == 'Delivered') {
+                    document.title = ' Pharmacy | Order Completed';
+                    $("[id*='dvOrderCompleted']").show();
+                    $("[id*='dvCenterUseOnly']").show();
+                    $("[id*='dvconfirmPaidBy']").show();
+                    $("[id*='dvEstimateDate']").hide();
+                    $("[id*='accordionFlushExample']").show();
+                    $("[id*='btnstatusname']").html('ORDER DELIVERED');
+                    $("[id*='btnstatusname']").attr('class', 'btn btn-orange');
                 }
 
-                $("#btnstatusname").text(data.results.statusname);
-                $("#txtMemberName").html('Hi, ' + data.results.memberName);
-                $("#btnGender").text(data.results.gender);
-                $("#txtAppointmentDate").html('<i class="fa-solid fa-calendar-check"></i> ' + data.results.appointmentDate);
-                $("#txtAppointmentTime").html('<i class="fa-solid fa-clock"></i> ' + data.results.appointmentTime);
-                $("#btnAge").text(data.results.memberAge + ' Years');
-                $("#btnCaseNo").text(data.results.caseNo);
-                $("#txtAppointmentAddress").html(place);
+                $("[id*='txtMemberName']").html('Hi, ' + data.results.memberName);
+                $("[id*='btnGender']").text(data.results.gender);
+                $("[id*='btnAge']").text(data.results.memberAge + ' Years');
+                $("[id*='txtAppointmentDate']").html('<img src="images/calendar-black.png" class="boxicon"> ' + data.results.estDate);
+                $("[id*='btnCaseNo']").text(data.results.caseNo);
+                $("[id*='txtAppointmentAddress']").html(place);
+                $("[id*='tagPrescription']").attr("href", data.results.reportSavePath);
+                $("[id*='txtOrderAmount']").text(data.results.orderAmount);
+                $("[id*='btnInvoice']").attr("href", data.results.invoiceLink);
+
+                if(data.results.serviceName == "Pharmacy"){
+                    $.each(data.results.coveredTests, function (key, value) {
+                        $("[id*='lstPharmacyOrder']").append('<li><i class="fa-solid fa-circle-check text-success"></i>' + ' '+ value.testName + '</a></li>');
+                    });
+                }
             } else {
                 console.log("Error in sucess");
                 window.location.replace("404.html");
